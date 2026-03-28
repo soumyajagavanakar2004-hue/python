27cf88f8470bdb359e57134d8bdbaf26239d406e
soumyajagavanakar2004-hue <soumyajagavanakar2004@gmail.com>
2026-03-28 09:28:46 +0530

    Add files via upload

---
+++ day 4.py
@@ -0,0 +1,63 @@
+# list=[7,3,9,2,8,]
+# list.sort()
+# print(list)
+# print(list[-2])
+
+# list=[0,1,0,3,12]
+# list.sort(reverse= True)
+# print(list)
+
+# list =[0,1,0,3,12]
+
+# for i in list:#i=0:0
+#     if i == 0:
+#         list.remove(i)
+#         list.append(i)
+# print(list)
+
+# A=[1,2,3] 
+# B=[2,3,4] 
+# C=[3,4,5] 
+
+# #for loop perform iteration
+# #take first element from A
+# #and check the element present in the B and C 
+# for i in A: #i=1
+#     if i in B and i in C:
+#         print(i)
+
+#while loop syntax:
+
+# even=0 #2
+# odd =0 #2
+# i=1 #i=4
+# while i<=10:
+#     if i%2 == 0:
+#         even = even + 1
+#     else:
+#         odd = odd + 1
+#     i = i+1
+# print("Even=",even)
+# print("Odd=",odd)
+
+#1+2+3+4+5+6+7+8+9+10 =55
+
+#WAP for this  using while loop
+# sum= 0 
+# i=1
+# while i <=10:
+#     sum = sum + i
+#     i = i+1
+# print("Sum=",sum)
+
+#1 5 
+#2 4 
+#4 2
+#5 1 
+
+# for i,j in zip(range(1,6), range(5,0,-1)): #i=1, j=5
+
+#     if i == 3 and j == 3:
+#         continue
+#     print(i ,"  ",j)
+
